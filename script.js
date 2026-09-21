4b6bdfbaff36a50e007d9dc45410e5ed95db2af0
jedavid33-design <jedavid33@gmail.com>
2026-09-21 14:45:26 -0400

    Build 209: remove literal backslash-n from N1 bindings

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const BUILD_VERSION = "208";
+  const BUILD_VERSION = "209";
   console.info(`Book OCR Studio ${BUILD_VERSION} loaded`);
 
   const $ = (id) => document.getElementById(id);
