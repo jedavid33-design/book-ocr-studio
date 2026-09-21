3eaa6f25ae1682ff3ef6bb1a070715c1f5c2b298
jedavid33-design <jedavid33@gmail.com>
2026-09-21 11:05:48 -0400

    Load Book OCR Studio build 196

--- script-loader.js
+++ script-loader.js
@@ -1,7 +1,7 @@
 // Book OCR Studio loader. Production source of truth is script.js.
 (() => {
   const script = document.createElement("script");
-  script.src = "./script.js?v=195";
+  script.src = "./script.js?v=196";
   script.async = false;
   script.onerror = () => console.error("Book OCR Studio loader failed to load script.js");
   document.head.appendChild(script);
