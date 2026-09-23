f93a65c837ff1f764070f6c53d1c51f9335dfbfb
jedavid33-design <jedavid33@gmail.com>
2026-09-23 15:19:41 -0400

    Build 240: bust runtime loader cache

--- script-loader.js
+++ script-loader.js
@@ -1,7 +1,7 @@
 // Book OCR Studio loader. Production source of truth is script.js.
 (() => {
   const script = document.createElement("script");
-  script.src = "./script.js?v=239";
+  script.src = "./script.js?v=240";
   script.async = false;
   script.onerror = () => console.error("Book OCR Studio loader failed to load script.js");
   document.head.appendChild(script);
